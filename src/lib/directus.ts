584736ba14d6dc236947637ca95bac0854c97f1c
codeit-ninja <richard@codeit.ninja>
2025-05-07 19:42:50 +0200

    Set default url to shut directus compiler error up

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -2,6 +2,6 @@ import { env } from "$env/dynamic/public";
 import { createDirectus, rest, staticToken } from "@directus/sdk";
 import type { MySchema } from "$directus";
 
-export const client = createDirectus<MySchema>(env.PUBLIC_DIRECTUS_URL)
+export const client = createDirectus<MySchema>(env.PUBLIC_DIRECTUS_URL || "https://api.mauritzschilderwerken.nl")
     .with(staticToken(env.PUBLIC_DIRECTUS_TOKEN))
     .with(rest())
